be915717a3cae77428dbf7cee087c2efa0682803
yohan9878 <yohanasitha98@gmail.com>
2024-08-29 11:47:42 +0530

    Update  service file

--- src/app/simulation-service.service.ts
+++ src/app/simulation-service.service.ts
@@ -3,11 +3,10 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MontyHallService {
-
-  private apiUrl = '';
+  private apiUrl = 'https://localhost:44338/api/simulate';
 
   constructor(private http: HttpClient) {}
 
